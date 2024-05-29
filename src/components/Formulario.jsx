3bc922b1ff25cca280507eabaf4d6682b4be02c3
Antonio-Mj <antonio.devpisc@gmail.com>
2024-05-29 14:09:58 -0600

    Update Formulario.jsx

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,3 +1,4 @@
+
 import "./Formulario.css";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -43,3 +44,46 @@ export function Formulario({ setUser }) {
         </>
     );
 }
+    import "./Formulario.css"
+    import { useState } from "react"
+
+    export function Formulario({setUser}) {
+
+        const [nombre, setNombre] = useState("")
+        const [contraseña, setContraseña] = useState("")
+        const [error, setError] = useState(false)
+
+        const handleSubmit = (e) => {
+            e.preventDefault()
+
+            if (nombre === "" || contraseña === ""){
+                setError(true)
+                return
+            }
+
+            setError(false)
+
+            setUser([nombre])
+        }
+
+
+        return (
+            <section>
+                <h1>Login</h1>
+
+                <form className="formulario"
+                onSubmit={handleSubmit}>
+                    <input type="text"
+                        value={nombre}
+                        onChange={e => setNombre(e.target.value)}
+                    />
+                    <input type="password"
+                        value={contraseña}
+                        onChange={e => setContraseña(e.target.value)}
+                    />
+                    <button>Iniciar sesión</button>
+                </form>
+                {error && <p>Todos los campos son obligatorios</p>}
+            </section>
+        )
+    }
