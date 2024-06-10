25bacf27cdb6411f7eb0ada43ec3b3f0491415ce
RafaelMurrieta <r7121005846@gmail.com>
2024-06-10 10:40:54 -0600

    Arreglo de base de datos

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,50 +1,69 @@
-import React, { useState } from "react"; 
-import "./Formulario.css";
-import { useNavigate } from "react-router-dom";
-import Imagen from "./Image.jsx";
+    import React, { useState } from "react";
+    import "./Formulario.css";
+    import { useNavigate } from "react-router-dom";
+    import Imagen from "./Image.jsx";
 
-export function Formulario({ setUser }) {
-    const [nombre, setNombre] = useState("");
-    const [contraseña, setContraseña] = useState("");
-    const [error, setError] = useState(false);
-    const navigate = useNavigate();
+    export function Formulario({ setUser }) {
+        const [nombre, setNombre] = useState("");
+        const [contraseña, setContraseña] = useState("");
+        const [error, setError] = useState(false);
+        const [loginError, setLoginError] = useState(false);
+        const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (nombre === "" || contraseña === "") {
-            setError(true);
-            return;
-        }
-        setError(false);
-        setUser([nombre]);
-        navigate("/home");
-    };
+        const handleSubmit = async (e) => {
+            e.preventDefault();
+            if (nombre === "" || contraseña === "") {
+                setError(true);
+                return;
+            }
+            setError(false);
 
-    return (
-        <>
-            <section className="section-form">
-                <div>
+            
+            const response = await fetch('http://localhost:3001/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ email: nombre, password: contraseña }),
+            });
+            const result = await response.json();
+            if (result.success) {
+                const data = JSON.parse(result.body);
+                const nameuser = data.name;
+                const id = data._id;
+                localStorage.setItem('coockieFill', id);
+                setUser([nameuser]);
+                navigate("/home");
+                
+            } else {
+                setLoginError(true);
+            }
+        };
+
+        return (
+            <>
+                <section className="section-form">
                     <div>
-                    {/* <img src="treedevisIcon.svg" alt="" /> */}
-                        <h1>TREENET</h1>
-                        
+                        <div>
+                            <h1>TREENET</h1>
+                        </div>
+                        <form className="formulario" onSubmit={handleSubmit}>
+                            <input type="email" placeholder="Correo electrónico"
+                                value={nombre}
+                                onChange={e => setNombre(e.target.value)}
+                            />
+                            <input type="password" placeholder="Contraseña"
+                                value={contraseña}
+                                onChange={e => setContraseña(e.target.value)}
+                            />
+                            <button>Iniciar sesión</button>
+                            <a href="/sing-create"><p>¿No tienes cuenta? Crea una</p></a>
+                        </form>
+                        {error && <p className="validation-camps">Todos los campos son obligatorios</p>}
+                        {loginError && <p className="validation-camps">Usuario no encontrado</p>}
                     </div>
-                    <form className="formulario" onSubmit={handleSubmit}>
-                        <input type="email" placeholder="Correo electrónico"
-                            value={nombre}
-                            onChange={e => setNombre(e.target.value)}
-                        />
-                        <input type="password" placeholder="Contraseña"
-                            value={contraseña}
-                            onChange={e => setContraseña(e.target.value)}
-                        />
-                        <button>Iniciar sesión</button>
-                        <a href="/sing-create"><p>¿No tienes cuenta? Crea una</p></a>
-                    </form>
-                    {error && <p className="validation-camps">Todos los campos son obligatorios</p>}
-                </div>
-            </section>
-            <Imagen />
-        </>
-    );
-}
+                </section>
+                <Imagen />
+            </>
+        );
+    }
