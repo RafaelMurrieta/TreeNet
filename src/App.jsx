821b9b0744a5e495b6fa1ce3c83395aec83fd179
Antonio-Mj <antonio.devpisc@gmail.com>
2024-06-14 11:24:50 -0600

    Componente para las publicaciones

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Formulario } from './components/Formulario.jsx';
 import { Home } from './components/Home.jsx';
 import { Createaccount } from './components/Createaccount.jsx';
 import NoFound from './components/NoFound.jsx';
+import { Posts } from './components/Posts.jsx';
 import './App.css';
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <Route path="/home" element={<Home user={user} setUser={setUser} />} />
         <Route path="*" element={<NoFound />} />
         <Route path='/sing-create' element={<Createaccount />} />
+        <Route path='/posts' element={<Posts />} />
       </Routes>
     </div>
   );
