35470d85d181acdb90e4a084deb514ca5d4ca8a2
Antonio-Mj <antonio.devpisc@gmail.com>
2024-06-14 16:54:04 -0600

    Contenedor para publicaciones

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { Formulario } from './components/Formulario.jsx';
 import { Home } from './components/Home.jsx';
 import { Createaccount } from './components/Createaccount.jsx';
 import NoFound from './components/NoFound.jsx';
-import { Posts } from './components/Posts.jsx';
 import './App.css';
 
 function App() {
@@ -18,7 +17,7 @@ function App() {
         <Route path="/home" element={<Home user={user} setUser={setUser} />} />
         <Route path="*" element={<NoFound />} />
         <Route path='/sing-create' element={<Createaccount />} />
-        <Route path='/posts' element={<Posts />} />
+        
       </Routes>
     </div>
   );
